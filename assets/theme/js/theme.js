a65e0e4e8f3b47c8a63f73f2e314ef8e0ae1748c
meng-xie <myxj128@gmail.com>
2017-11-22 21:15:50 -0800

    32

--- assets/theme/js/theme.js
+++ assets/theme/js/theme.js
@@ -48,7 +48,7 @@
             type: 'POST',
             data: params,
             crossDomain : true,
-             
+            dataType: "json", // what type of data do we expect back from the server
             cache: false,
             async: false,
             headers: { 'Access-Control-Allow-Origin': '*' ,
