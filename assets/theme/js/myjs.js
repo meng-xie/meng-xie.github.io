d50b79325ed2af4a6741e1d5e4e0efe66cea8d81
meng xie <myxj128@gmail.com>
2024-11-15 11:52:00 -0800

    google map bug fix

--- assets/theme/js/myjs.js
+++ assets/theme/js/myjs.js
@@ -5,33 +5,37 @@ function myprint(){
 }
 
 
-function openMap() {
-    var earthquakeAuth = {lat:37.335480, lng:-121.893028};
-           
-     var map = new google.maps.Map(document.getElementById('map-contact'), {
-        zoom:12,
-        center: earthquakeAuth
-    });
+  (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+    key: "AIzaSyDJSzXb9C3t1QHw0zqluXkn8xQbmL2RiCE",
+    v: "weekly",
+    // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+    // Add other bootstrap parameters as needed, using camel case.
+  });
 
-    var marker = new google.maps.Marker({
-    position: earthquakeAuth,
-    map: map
-    });
+ // Initialize and add the map
+ let map;
 
-    var contentString = 
- 
-    	 ' <div  >'+
-    	 '  <h4 class="no-margin-top font-alt">I am Here</h4>'+
-    	 '  <p><i class="fa fa-map-marker"></i> <strong>Address:</strong> San Francisco</p>'+
-    	 '  <p><i class="fa fa-mobile"></i> <strong>Phone:</strong> 415-629-1552 </p>'+
-    	 ' <p><i class="fa fa-envelope"></i> <strong>Email:</strong> myxj128@gmail.com</p>'+
- 
-    	 ' </div>';
-    var infowindow = new google.maps.InfoWindow({
-           content: contentString
-         });
+ async function initMap() {
+   // The location of Uluru
+   const position = { lat: 37.2844257, lng: -121.8691641 };
+   // Request needed libraries.
+   //@ts-ignore
+   const { Map } = await google.maps.importLibrary("maps");
+   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-         marker.addListener('click', function() {
-          infowindow.open(map, marker);
-        });
-}
+   // The map, centered at Uluru
+   map = new Map(document.getElementById("map-contact"), {
+     zoom: 12,
+     center: position,
+     mapId: "DEMO_MAP_ID",
+   });
+
+   // The marker, positioned at Uluru
+   const marker = new AdvancedMarkerElement({
+     map: map,
+     position: position,
+     title: "Uluru",
+   });
+ }
+
+ initMap();
